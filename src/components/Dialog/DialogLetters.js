3c7f16c055fb5d00aab7186ec0a060c26b4f680f
Lucas <brittosmonteiro@gmail.com>
2022-11-01 10:01:13 -0300

    Add verification

--- src/components/Dialog/DialogLetters.js
+++ src/components/Dialog/DialogLetters.js
@@ -10,34 +10,44 @@ export default function DialogLetter(props) {
         title="Letters"
       >
         <EnvelopeSimple className="icon-sm icon-white-1" />
-        <span className="text-white-1 font-sm font-light">
-          ({props.letters.length})
-        </span>
+        {props.letters ? (
+          <span className="text-white-1 font-sm font-light">
+            ({props.letters.length})
+          </span>
+        ) : null}
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="overlay">
           <Dialog.Content className="dialog text-white-1">
-            <Accordion.Root type="single" collapsible className="column">
-              {props.letters.map((letter, key) => (
-                <div key={letter.id}>
-                  <Accordion.Item
-                    key={letter.id}
-                    value={letter.id}
-                    className="border-radius-soft pa-2"
-                  >
-                    <Accordion.Header>
-                      <Accordion.Trigger className="bg-transparent font-md text-left text-white-1 first-letter-capitalize">
-                        {letter.title}
-                      </Accordion.Trigger>
-                    </Accordion.Header>
-                    <Accordion.Content className="note font-sm mt-4 pa-2 border-radius-soft border-default bg-transparent text-white-1  first-letter-capitalize">
-                      {letter.body}
-                    </Accordion.Content>
-                  </Accordion.Item>
-                  {key < props.letters.length - 1 ? <hr className="mt-2" /> : null}
-                </div>
-              ))}
-            </Accordion.Root>
+            {props.letters ? (
+              <Accordion.Root type="single" collapsible className="column">
+                {props.letters.map((letter, key) => (
+                  <div key={letter.id}>
+                    <Accordion.Item
+                      key={letter.id}
+                      value={letter.id}
+                      className="border-radius-soft pa-2"
+                    >
+                      <Accordion.Header>
+                        <Accordion.Trigger className="bg-transparent font-md text-left text-white-1 first-letter-capitalize">
+                          {letter.title}
+                        </Accordion.Trigger>
+                      </Accordion.Header>
+                      <Accordion.Content className="note font-sm mt-4 pa-2 border-radius-soft border-default bg-transparent text-white-1  first-letter-capitalize">
+                        {letter.body}
+                      </Accordion.Content>
+                    </Accordion.Item>
+                    {key < props.letters.length - 1 ? (
+                      <hr className="mt-2" />
+                    ) : null}
+                  </div>
+                ))}
+              </Accordion.Root>
+            ) : (
+              <p className="no-data pa-4 border-radius-sm font-light font-sm text-center">
+                It was not possible to load posts. <br /> Try again later.
+              </p>
+            )}
           </Dialog.Content>
         </Dialog.Overlay>
       </Dialog.Portal>
--- src/components/Dialog/DialogUser.js
+++ src/components/Dialog/DialogUser.js
@@ -1,6 +1,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import * as Accordion from "@radix-ui/react-accordion";
 import { User } from "phosphor-react";
+import React from "react";
 
 export default function DialogUser(props) {
   return (
@@ -14,140 +15,149 @@ export default function DialogUser(props) {
       <Dialog.Portal>
         <Dialog.Overlay className="overlay">
           <Dialog.Content className="dialog text-white-1">
-            <Dialog.Title className="font-medium font-md">
-              {props.profile.name}
-            </Dialog.Title>
-            <Accordion.Root
-              type="single"
-              collapsible
-              className="column mt-4"
-              defaultValue="personal"
-            >
-              <Accordion.Item className="mt-1" value="personal">
-                <Accordion.Header>
-                  <Accordion.Trigger className="bg-transparent font-md text-left text-white-1 first-letter-capitalize">
-                    Personal information
-                  </Accordion.Trigger>
-                </Accordion.Header>
-                <Accordion.Content>
-                  <div className="row mt-4">
-                    <div className="column">
-                      <label htmlFor="email">Username</label>
-                      <input
-                        type={"text"}
-                        defaultValue={props.profile.username}
-                        className="input pa-2 font-sm border-radius-soft border-default"
-                        disabled={true}
-                      />
-                    </div>
-                  </div>
+            {props.profile ? (
+              <React.Fragment>
+                <Dialog.Title className="font-medium font-md">
+                  {props.profile.name}
+                </Dialog.Title>
+                <Accordion.Root
+                  type="single"
+                  collapsible
+                  className="column mt-4"
+                  defaultValue="personal"
+                >
+                  <Accordion.Item className="mt-1" value="personal">
+                    <Accordion.Header>
+                      <Accordion.Trigger className="bg-transparent font-md text-left text-white-1 first-letter-capitalize">
+                        Personal information
+                      </Accordion.Trigger>
+                    </Accordion.Header>
+                    <Accordion.Content>
+                      <div className="row mt-4">
+                        <div className="column">
+                          <label htmlFor="email">Username</label>
+                          <input
+                            type={"text"}
+                            defaultValue={props.profile.username}
+                            className="input pa-2 font-sm border-radius-soft border-default"
+                            disabled={true}
+                          />
+                        </div>
+                      </div>
 
-                  <div className="row mt-4">
-                    <div className="column">
-                      <label htmlFor="email">Email</label>
-                      <input
-                        type={"text"}
-                        defaultValue={props.profile.email}
-                        className="input pa-2 font-sm border-radius-soft border-default"
-                        disabled={true}
-                      />
-                    </div>
-                  </div>
+                      <div className="row mt-4">
+                        <div className="column">
+                          <label htmlFor="email">Email</label>
+                          <input
+                            type={"text"}
+                            defaultValue={props.profile.email}
+                            className="input pa-2 font-sm border-radius-soft border-default"
+                            disabled={true}
+                          />
+                        </div>
+                      </div>
 
-                  <div className="row mt-4">
-                    <div className="column">
-                      <label htmlFor="email">Website</label>
-                      <input
-                        type={"text"}
-                        defaultValue={props.profile.website}
-                        className="input pa-2 font-sm border-radius-soft border-default"
-                        disabled={true}
-                      />
-                    </div>
-                  </div>
+                      <div className="row mt-4">
+                        <div className="column">
+                          <label htmlFor="email">Website</label>
+                          <input
+                            type={"text"}
+                            defaultValue={props.profile.website}
+                            className="input pa-2 font-sm border-radius-soft border-default"
+                            disabled={true}
+                          />
+                        </div>
+                      </div>
 
-                  <div className="row mt-4">
-                    <div className="column">
-                      <label htmlFor="email">Phone:</label>
-                      <input
-                        type={"text"}
-                        defaultValue={props.profile.phone}
-                        className="input pa-2 font-sm border-radius-soft border-default"
-                        disabled={true}
-                      />
-                    </div>
-                  </div>
-                </Accordion.Content>
-              </Accordion.Item>
+                      <div className="row mt-4">
+                        <div className="column">
+                          <label htmlFor="email">Phone:</label>
+                          <input
+                            type={"text"}
+                            defaultValue={props.profile.phone}
+                            className="input pa-2 font-sm border-radius-soft border-default"
+                            disabled={true}
+                          />
+                        </div>
+                      </div>
+                    </Accordion.Content>
+                  </Accordion.Item>
 
-              <hr className="my-4" />
+                  <hr className="my-4" />
 
-              <Accordion.Item value="address">
-                <Accordion.Header>
-                  <Accordion.Trigger className="bg-transparent font-md text-left text-white-1 first-letter-capitalize">
-                    Address
-                  </Accordion.Trigger>
-                </Accordion.Header>
-                <Accordion.Content>
-                  <div className="row mt-4">
-                    <div className="column">
-                      <label htmlFor="email">Street</label>
-                      <input
-                        type={"text"}
-                        defaultValue={props.profile.address.street}
-                        className="input pa-2 font-sm border-radius-soft border-default"
-                        disabled={true}
-                      />
-                    </div>
-                  </div>
-                  <div className="row mt-4">
-                    <div className="column">
-                      <label htmlFor="email">Suite</label>
-                      <input
-                        type={"text"}
-                        defaultValue={props.profile.address.suite}
-                        className="input pa-2 font-sm border-radius-soft border-default"
-                        disabled={true}
-                      />
-                    </div>
-                  </div>
-                  <div className="row mt-4">
-                    <div className="column">
-                      <label htmlFor="email">City</label>
-                      <input
-                        type={"text"}
-                        defaultValue={props.profile.address.city}
-                        className="input pa-2 font-sm border-radius-soft border-default"
-                        disabled={true}
-                      />
-                    </div>
-                  </div>
-                </Accordion.Content>
-              </Accordion.Item>
+                  <Accordion.Item value="address">
+                    <Accordion.Header>
+                      <Accordion.Trigger className="bg-transparent font-md text-left text-white-1 first-letter-capitalize">
+                        Address
+                      </Accordion.Trigger>
+                    </Accordion.Header>
+                    <Accordion.Content>
+                      <div className="row mt-4">
+                        <div className="column">
+                          <label htmlFor="email">Street</label>
+                          <input
+                            type={"text"}
+                            defaultValue={props.profile.address.street}
+                            className="input pa-2 font-sm border-radius-soft border-default"
+                            disabled={true}
+                          />
+                        </div>
+                      </div>
+                      <div className="row mt-4">
+                        <div className="column">
+                          <label htmlFor="email">Suite</label>
+                          <input
+                            type={"text"}
+                            defaultValue={props.profile.address.suite}
+                            className="input pa-2 font-sm border-radius-soft border-default"
+                            disabled={true}
+                          />
+                        </div>
+                      </div>
+                      <div className="row mt-4">
+                        <div className="column">
+                          <label htmlFor="email">City</label>
+                          <input
+                            type={"text"}
+                            defaultValue={props.profile.address.city}
+                            className="input pa-2 font-sm border-radius-soft border-default"
+                            disabled={true}
+                          />
+                        </div>
+                      </div>
+                    </Accordion.Content>
+                  </Accordion.Item>
 
-              <hr className="my-4" />
+                  <hr className="my-4" />
 
-              <Accordion.Item value="company">
-                <Accordion.Header>
-                  <Accordion.Trigger className="bg-transparent font-md text-left text-white-1 first-letter-capitalize">
-                    Company
-                  </Accordion.Trigger>
-                </Accordion.Header>
-                <Accordion.Content>
-                  <div className="row mt-4">
-                    <div className="column">
-                      <label htmlFor="email">Name</label>
-                      <input
-                        type={"text"}
-                        defaultValue={props.profile.company.name}
-                        className="input pa-2 font-sm border-radius-soft border-default"
-                        disabled={true}
-                      />
-                    </div>
-                  </div>
-                </Accordion.Content>
-              </Accordion.Item>
-            </Accordion.Root>
+                  <Accordion.Item value="company">
+                    <Accordion.Header>
+                      <Accordion.Trigger className="bg-transparent font-md text-left text-white-1 first-letter-capitalize">
+                        Company
+                      </Accordion.Trigger>
+                    </Accordion.Header>
+                    <Accordion.Content>
+                      <div className="row mt-4">
+                        <div className="column">
+                          <label htmlFor="email">Name</label>
+                          <input
+                            type={"text"}
+                            defaultValue={props.profile.company.name}
+                            className="input pa-2 font-sm border-radius-soft border-default"
+                            disabled={true}
+                          />
+                        </div>
+                      </div>
+                    </Accordion.Content>
+                  </Accordion.Item>
+                </Accordion.Root>
+              </React.Fragment>
+            ) : (
+              <p className="no-data pa-4 border-radius-sm font-light font-sm text-center">
+                It was not possible to show person's profile. <br /> Try again
+                later.
+              </p>
+            )}
           </Dialog.Content>
         </Dialog.Overlay>
       </Dialog.Portal>
